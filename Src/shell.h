bd5bd04f5a9b3020a5759721e708925df5f805bd
Quist <joakiml@gmail.com>
2012-11-20 18:51:23 -0800

    Moved code to src folder

---
+++ Src/shell.h
@@ -0,0 +1,31 @@
+
+using namespace std;
+
+#include <cstdlib>
+#include <string>
+#include <iostream>
+#include <unistd.h>
+
+//Program includes:
+#include "history.h"
+
+class Shell{
+    
+ public:
+    void orderLoop();
+    Shell();
+    
+private:
+    char currentPath[1024]; 
+    
+    void updateCurrentPath();
+    void handleUserInput(string userInput);
+    
+    void startProcess();
+    
+    
+    
+    //Shell commands:
+    string cmdSetPath;
+    string cmdSetDataPath;    
+};

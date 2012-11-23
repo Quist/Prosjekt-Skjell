acc66aed48a7d383f313eb4773b9f36cf3487556
Quist <joakiml@gmail.com>
2012-11-22 18:22:26 -0800

    Added datastructure for job

--- src/job.h
+++ src/job.h
@@ -4,12 +4,17 @@ using namespace std;
 #include <cstdlib>
 
 //Program includes:
+#include "process.h"
 
-
+/* Structure for a job.
+ * Processes in the job are stored in a linked list.
+ */
 class Job{
     
  public:
     
 private:
+    Process *firstProcess;
+    Job *nextJob;
 
 };
